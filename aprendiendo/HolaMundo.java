302f939e68b719a11edbf9a5e6e5a1d8c7d0c9f4
ESLEDA <esleda.kyo@gmail.com>
2025-03-12 18:20:13 -0600

    good bye

--- aprendiendo/HolaMundo.java
+++ aprendiendo/HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!"); // Imprime el mensaje en la consola
         System.out.println("hi");
+        System.out.println("good bye");
     }
 }
