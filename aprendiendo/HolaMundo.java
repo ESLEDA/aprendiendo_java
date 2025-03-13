d9564f729a4f2fc81f8970a37f113998f3f3d14b
ESLEDA <esleda.kyo@gmail.com>
2025-03-12 18:18:25 -0600

    hi

--- aprendiendo/HolaMundo.java
+++ aprendiendo/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!"); // Imprime el mensaje en la consola
+        System.out.println("hi");
     }
 }
--- aprendiendo/Main.java
+++ aprendiendo/Main.java
@@ -6,5 +6,6 @@ public class Main{
     public static void main(String[] args) {
         int[][] numeros = {{0,1},{2,3}};
         System.out.println(Arrays.toString(numeros));
+        
     }
 }
